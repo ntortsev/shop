c09d44ca84fad4a6b73d46a3257bb8107ef23b50
Yan Kahanov <kahanov.yan@gmail.com>
2022-01-05 14:18:16 +0500

    added empty basket

--- src/components/basket/index.tsx
+++ src/components/basket/index.tsx
@@ -1,12 +1,15 @@
-import { Drawer } from 'antd'
+import { Drawer, Empty } from 'antd'
 import React from 'react'
 import BasketItem from './item'
 import DrawerFooter from '../drawer-footer';
 import useModal from '../../hooks/use-modal'
 import basket from '../../store/basket';
+import styled from 'styled-components';
+import { observer } from 'mobx-react-lite';
 
 const Basket = () => {
     const [isVisible, handleClose] = useModal('/basket');
+    const isBasketEmpty = basket.list.length === 0
 
     return (
         <Drawer 
@@ -14,20 +17,32 @@ const Basket = () => {
         placement="right" 
         visible={isVisible}
         onClose={handleClose}
-        footer={<DrawerFooter />}
+        footer={!isBasketEmpty && <DrawerFooter />}
         style={DrawerStyles}
         >
-            {basket.list.map((item) => (
+            {!isBasketEmpty ? 
+            basket.list.map((item) => (
                 <BasketItem 
                 key={item.id}
                 {...item}
                 />
-            ))}
+            ))
+            : <EmptyWrap>
+                <Empty />
+            </EmptyWrap>
+            }
         </Drawer>
     )
 }
 
-export default Basket
+export default observer(Basket)
+
+const EmptyWrap = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const DrawerStyles: React.CSSProperties = {
     zIndex: 2000
