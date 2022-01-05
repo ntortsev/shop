2afced2b88b00d968a20b90a94163aeec056a42b
Yan Kahanov <kahanov.yan@gmail.com>
2022-01-05 15:02:44 +0500

    fixed bug with modals

--- src/hooks/use-modal.ts
+++ src/hooks/use-modal.ts
@@ -1,20 +1,22 @@
 import React from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
+import useBlockUrls from './use-block-urls';
 
 const useModal = (currPath: string): [boolean, () => void] => {
     const navigate = useNavigate();
     const location = useLocation();
     const [isVisible, setIsVisible] = React.useState(false)
     const [prevLocation, setPrevLocation] = React.useState('/')
+    const blockUrls = useBlockUrls()
 
     const handleClose = () => {
         navigate(prevLocation)
     }
 
     React.useEffect(() => {
-        if(location.pathname !== currPath && location.pathname !== '/payment'){
+        blockUrls(() => {
             setPrevLocation(location.pathname+location.search)
-        } 
+        }) 
         setIsVisible(location.pathname === currPath)
     }, [location])
 
